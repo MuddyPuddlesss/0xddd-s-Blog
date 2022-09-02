59ddd3de3240474490ad086b564d009408c52e99
JesseX <shallow.wow@gmail.com>
2022-09-02 23:58:03 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/MuddyPuddlesss/0xddd-s-Blog.git master:master
+git push -f https://github.com/MuddyPuddlesss/0xddd-s-Blog.git blog:blog
 
 cd -
